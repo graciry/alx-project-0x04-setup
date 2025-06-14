52894d3cc7cd5b4e30955693aa8e64d7d89c6c35
graciry <ngunyigwothaya@gmail.com>
2025-06-14 23:51:49 +0300

    Fix: remove ghost files, keep only two folders

--- components/layouts/Layout.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { LayoutProps } from "@/interface";
-
-const Layout: React.FC<LayoutProps> = ({ children }) => {
-  return <div className="layout-container">{children}</div>;
-};
-
-export default Layout;
